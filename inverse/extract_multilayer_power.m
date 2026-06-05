5617db4b206547440a3beba38e9a9388b9c54a0d
JanSchalla <jan.schalla@hhu.de>
2026-06-05 15:45:55 +0200

    Create raw power comparison

---
+++ inverse/extract_multilayer_power.m
@@ -0,0 +1,260 @@
+function extract_multilayer_power(sFiles, protocol_name, params)
+
+if ~brainstorm('status')
+    brainstorm nogui
+end
+
+% Parse inputs
+if exist('params', 'var') && ~isempty(params)
+    if isfield(params, 'extract_params')
+        extract_params = params.extract_params;
+    end
+end
+
+ % Open protocol
+protocol = bst_get('Protocol', protocol_name);
+gui_brainstorm('SetCurrentProtocol', protocol);
+
+tf_template = db_template('timefreq');
+tf_template.DataType = 'results';
+
+% sFiles cell of relaitves pathes to the trial data from the brainstorm db
+sData = in_bst_data(sFiles{1});
+time = sData.Time;
+clear sData
+%% 1st: Let user define baseline, window of interest (woi) and frequency of interest (foi).
+if ~exist("extract_params", "var")
+    dlgTitle = 'Specify multilayer extraction parameter';
+    fieldsize = [10 50];
+    prompt = 'freq id/ freq range / WOI';
+    example = {'theta / 4, 8 / 0, 1'};
+    answer = inputdlg(prompt, dlgTitle, fieldsize, example);
+
+    jobs = cellstr(answer{:});
+else
+    jobs = cellstr(extract_params);
+end
+
+n_jobs = numel(jobs);
+
+fprintf('Number of jobs: %i\n', n_jobs);
+
+% Identify jobs which need the same filtering to reduce computation demands
+pair = zeros(n_jobs, 2);
+
+for i = 1:n_jobs
+    tok = regexp(jobs{i}, '/\s*([\d]+),\s*([\d]+)\s*/', 'tokens', 'once');
+    pair(i, :) = str2double(tok);
+end
+
+[G, ~] = findgroups(pair(:, 1), pair(:, 2));
+
+independend_jobs = [];
+n_sub_jobs = [];
+
+for g = 1:numel(G)
+    idx = find(G == g);
+
+    if ~isempty(idx)
+        independend_jobs(end+1, :) = unique(pair(idx, :));
+        n_sub_jobs(end+1) = size(pair(idx, :), 1);
+    end
+end
+
+fprintf('Number of independend jobs: %i\n', size(independend_jobs, 1));
+
+sfreq = round(1/(time(2) - time(1)));
+%% Check if all files are of the same run
+nTrialsTotal = length(sFiles);
+ses_ids = unique(cellfun(@fileparts, sFiles, 'UniformOutput', false));
+% group files in sessions
+session_files = cell(1, length(ses_ids));
+for ses = 1:length(ses_ids)
+    file_mask = cellfun(@(x) contains(x, ses_ids{ses}), sFiles);
+    session_files{ses} = {sFiles{file_mask}};
+end
+
+fprintf('%i Sessions identfied.\n', length(ses_ids));
+%tic
+for ses=1:length(ses_ids)
+    fprintf('Working on Session: %i\n', ses);
+
+    % init brainstorm
+    bstrm_path = session_files{ses}{1};
+    if length(split(bstrm_path, "/")) > length(split(bstrm_path, "\"))
+        separator = "/";
+    else
+        separator = "\";
+    end
+    tokens = split(bstrm_path, separator);
+    protocol_id = find(ismember(tokens, "brainstorm_db"), 1) + 1;
+    % check if previous path was relative
+    if isempty(protocol_id)
+        bstrm_path = file_fullpath(bstrm_path);
+        % Redo if previous path was relative
+        tokens = split(bstrm_path, separator);
+    end
+
+    subject_id = find(ismember(tokens, "data"), 1) + 1;
+    subject_name = convertStringsToChars(tokens{subject_id});
+    
+    bstrm_out_path = fileparts(bstrm_path);
+    % find sr file
+    kernel_file = dir(fullfile(strjoin(tokens(1:subject_id+1), separator), '*KERNEL*.mat'));
+    if isempty(kernel_file)
+        fprintf('No source reconstruction found for %s. Cancelling process ...', subject_name);
+        return
+    else
+        if length(kernel_file) == 1
+            kernel_file = string(fullfile(kernel_file.folder, kernel_file.name));
+            used_SR(ses) = kernel_file;
+        else
+            % Prepare list of full file paths and display names
+            file_paths = fullfile({kernel_file.folder}, {kernel_file.name});
+            file_names = {kernel_file.name};
+            
+            [selection, is_ok] = listdlg('PromptString', sprintf('Multiple KERNEL files found for %s.\nSelect one:', subject_name), ...
+                                         'SelectionMode', 'single', ...
+                                         'ListString', file_names, ...
+                                         'Name', 'Choose Kernel File');
+            
+            if is_ok && ~isempty(selection)
+                kernel_file = string(file_paths{selection});
+                used_SR(ses) = kernel_file;
+                fprintf('Selected kernel file: %s\n', kernel_file);
+            else
+                fprintf('No kernel file selected for %s. Cancelling process ...\n', subject_name);
+                return
+            end
+        end
+    end
+
+    sKernel = load(kernel_file);
+    
+    nSources = size(sKernel.ImagingKernel, 1);
+    nVertices = nSources/2;
+    
+    nSources_white = 1:nVertices;
+    nSources_pial = nVertices+1:nSources;
+    
+    if length(nSources_pial) ~= length(nSources_white)
+        error('White and pial subsurfaces contain different nr. of vertices! Aborting...');
+    end
+
+    sesTrials = length(session_files{ses});
+    
+    % Populate output structure
+    ses_template = tf_template;
+    ses_template.HeadModelFile = sKernel.HeadModelFile;
+    ses_template.SurfaceFile = sKernel.SurfaceFile;
+    ses_template.RowNames = [1:nSources];
+    ses_template.Comment = ses_ids{ses};
+    ses_template.TF = zeros(nSources, 1, length(jobs));
+    ses_template.Time = [min(time), max(time)];
+    ses_template.Measure = 'Power';
+    ses_template.Method = 'hilbert';
+    ses_template.nAvg = 1;
+    ses_template.Leff = 1;
+
+    disp('Starting extraction ...')
+    fprintf('Progress: %3d%%\n', 0);
+    for iTrial = 1:sesTrials
+        fprintf(1, '\b\b\b\b%3.0f%%', 100*(iTrial/sesTrials));
+
+        trial_template = ses_template;
+
+        % Load data for each trial
+        [~, trial_id] = fileparts(session_files{ses}{iTrial});
+        sRawData = in_bst_data(session_files{ses}{iTrial});
+    
+        % Reduce sensor data to only used channels in source reconstruction
+        sensorData = sRawData.F(sKernel.GoodChannel, :);
+
+        job_idx = 0;
+        
+        for job = 1:size(independend_jobs, 1)
+
+            % Bandpasss filter sensor data
+            sensorData_bp = bandpass(sensorData', [independend_jobs(job, :)], sfreq)';
+
+            for sub_job=1:n_sub_jobs(job)
+
+                % Update job idx to store data
+                job_idx = job_idx + 1;
+            
+                %% Initialize current extraction params
+                job_specs = split(jobs{job_idx}, '/');
+                job_specs = cellfun(@(x) x(find(~isspace(x))), job_specs, 'UniformOutput', false);
+            
+                job_struct = struct( ...
+                    'id', job_specs{1}, ...
+                    'freq_range', str2double(split(job_specs{2}, ',')), ...
+                    'woi', str2double(split(job_specs{3}, ',')));
+            
+            
+                if max(job_struct.woi) > max(time)
+                    error('Window of Interest ends outside of trial bound. WOI cannot be defined bigger then %.3f seconds (Job ID: %s)', max(time), job_struct.id);
+                end
+                
+                if max(job_struct.freq_range) > sfreq/2
+                    error('Selected Frequency of Interest is bigger then Nyquist Frequency (%i Hz).', round(sfreq/2));
+                end
+                            
+                if ~isfield(trial_template, 'Freqs')
+                    % Update Frequency Bins
+                    trial_template.Freqs{1, 1} = char(job_struct.id);
+                    trial_template.Freqs{1, 2} = char(join(string(job_struct.freq_range), ','));
+                    trial_template.Freqs{1, 3} = char(sprintf('WOI: %.2f-%.2f', job_struct.woi));
+                    
+                    % Update history
+                    trial_template.History{1, 1} = char(datestr(now, 'dd/mm/yy-HH:MM'));
+                    trial_template.History{1, 2} = char('compute'); 
+                    trial_template.History{1, 3} = char(sprintf('extract_multilayer_power | %s; WOI: %f %f; Freq: %i %i', job_struct.id, job_struct.woi, job_struct.freq_range));
+                else 
+                    freq_id = size(trial_template.Freqs, 1);
+                    % Update Frequency bins
+                    trial_template.Freqs{freq_id+1, 1} = char(job_struct.id);
+                    trial_template.Freqs{freq_id+1, 2} = char(join(string(job_struct.freq_range), ','));
+                    trial_template.Freqs{freq_id+1, 3} = char(sprintf('WOI: %.2f-%.2f', job_struct.woi));
+                    
+                    % Update history
+                    trial_template.History{freq_id+1, 1} = char(datestr(now, 'dd/mm/yy-HH:MM'));
+                    trial_template.History{freq_id+1, 2} = char('compute'); 
+                    trial_template.History{freq_id+1, 3} = char(sprintf('extract_multilayer_power | %s; WOI: %f %f; Freq: %i %i', job_struct.id, job_struct.woi, job_struct.freq_range));
+                end
+                
+                % bring time information in sample space
+                [~, woi_min_idx] = min(abs(time - job_struct.woi(1)));
+                [~, woi_max_idx] = min(abs(time - job_struct.woi(2)));
+                woi_samples = [woi_min_idx woi_max_idx];
+                clear('woi_min_idx', "woi_max_idx");
+    
+                % extract job specific data
+                source_data_woi = sKernel.ImagingKernel * sensorData_bp(:, woi_samples(1):woi_samples(2));
+               
+                % In the original lameg code (from Jimmy) the absolute hilbert
+                % is not squared. Here we square to have an assessment of
+                % instantaneous power.
+                source_power_woi = mean(abs(hilbert(source_data_woi)).^2, 2);
+                                
+                trial_template.TF(:, 1, job_idx) = source_power_woi;
+    
+                trial_template.Comment = char(join([trial_template.Comment trial_id "multilayer power"], " | "));
+                trial_template.History{end+1, 1} = char(datestr(now, 'dd/mm/yy-HH:MM'));
+                trial_template.History{end, 2} = char('compute'); 
+                trial_template.History{end, 3} = char(sprintf('extract_multilayer_power | %s; WOI: %f %f; Freq: %i %i', job_struct.id, job_struct.woi, job_struct.freq_range));
+            end % sub_job
+        end % job
+        
+        % Link it to the trial sensor data and to the multilayer kernel!
+        trial_template.DataFile = char(sprintf('link|%s|%s', file_short(char(kernel_file)), session_files{ses}{iTrial}));
+        
+        out_fname = sprintf('timefreq_%s_psd_multilayer_power.mat', trial_id(end-7:end));
+        
+        save(fullfile(bstrm_out_path, out_fname), '-struct', 'trial_template');
+    end % iTrial
+
+
+    fprintf('\nExtraction finished for Session %i!\n', ses);
+end % ses
+%toc
