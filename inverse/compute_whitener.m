bd329d49bc44749bd9dd1d7fa33b828f37a77a83
JanSchalla <jan.schalla@hhu.de>
2026-06-28 14:59:20 +0200

    Create compute_whitener (brainstorm like)

---
+++ inverse/compute_whitener.m
@@ -0,0 +1,20 @@
+function [Cy, iW] = compute_whitener(Cy)
+
+[Un, Sn2] = svd(Cy, 'econ');
+Sn = sqrt(diag(Sn2));
+tol = length(Sn) * eps(single(Sn(1)));
+Rank_Noise = sum(Sn > tol);
+
+% Rebuild cov
+Un = Un(:, 1:Rank_Noise);
+Sn = Sn(1:Rank_Noise);
+Cy = Un * diag(Sn.^2)*Un';
+
+% Regularize (In this case by median Singluar value; like in LCMV from
+% bstrm)
+Sn = max(Sn, median(Sn));
+% Rebuild cov again
+Cy = Un * diag(Sn.^2) * Un';
+
+% Build Whitener
+iW = Un * diag(1./Sn)*Un';
