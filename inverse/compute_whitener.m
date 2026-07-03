0818ad8d38aef12eebfda59ef791fefa908dc2fc
JanSchalla <jan.schalla@hhu.de>
2026-07-03 14:04:51 +0200

    Update: Add regularization methods

--- inverse/compute_whitener.m
+++ inverse/compute_whitener.m
@@ -1,4 +1,15 @@
-function [Cy, iW] = compute_whitener(Cy)
+function iW = compute_whitener(Cy, params)
+
+method = 'none';
+
+if exist('params', 'var')
+    if isfield(params, 'method')
+        method = params.method;
+    end
+    if isfield(params, 'NoiseReg')
+        NoiseReg = params.NoiseReg;
+    end
+end
 
 [Un, Sn2] = svd(Cy, 'econ');
 Sn = sqrt(diag(Sn2));
@@ -8,15 +19,28 @@ Rank_Noise = sum(Sn > tol);
 % Rebuild cov
 Un = Un(:, 1:Rank_Noise);
 Sn = Sn(1:Rank_Noise);
-Cy = Un * diag(Sn.^2)*Un';
 
-% Regularize (In this case by median Singluar value; like in LCMV from
-% bstrm)
-Sn = max(Sn, median(Sn));
-% Rebuild cov again
-Cy = Un * diag(Sn.^2) * Un';
 
-% Build inverse whitener 
-iW = Un * diag(1./Sn) * Un';
-% W = Un * diag(Sn) * Un';
-% I = W * iW
+% Bild whitener. s^-0.5 = 1/sqrt(s) -> before we took already the sqrt(s),
+% so now computing the whitener as u * (1./s) * u' = u * s^-0.5 * u'!!
+if strcmp(method, 'none')
+    iW = Un * diag(1./Sn) * Un';
+elseif strcmp(method, 'median')
+    % Regularize (In this case by median Singluar value; like in LCMV from
+    % bstrm)
+    Sn = max(Sn, median(Sn));
+    
+    % Build inverse whitener 
+    iW = Un * diag(1./Sn) * Un';
+    % W = Un * diag(Sn) * Un';
+    % I = W * iW
+elseif strcmp(method, 'reg')
+    if ~exist('NoiseReg', 'var')
+        error('For the method "reg", please specify a NoiseReg parameter!');
+    end
+
+    RidgeFactor = mean(diag(Sn2)) * NoiseReg; % Hamalainen's preferred measure
+
+    iW = Un*diag(1./sqrt(Sn.^2 + RidgeFactor))*Un'; % inverse whitener, symmetric
+end
+
